82b734b997fc9d7cc78b451830a9edf37f7ca243
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2014-11-15 16:55:05 -0500

    added module docs

--- lib/generator.ex
+++ lib/generator.ex
@@ -1,4 +1,11 @@
 defmodule DataGenerator do
+	@moduledoc """
+	Generates test data files.
+	"""
+
+	@doc """
+	Creates a file of data with number_of_rows rows, each value separated by the delimiter. The file is written to filename.
+	"""
 	def generate_file(filename, delimiter, number_of_rows) do
 		{:ok, file} = File.open filename, [:write]	
 		row = Enum.join(["this", "is", "a", "row", "of", "test", "data"], delimiter)
