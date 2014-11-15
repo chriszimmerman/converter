ee290e27bada9c1d562cf46cd54733ad40ab5f13
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2014-11-14 22:44:18 -0500

    Renamed convert_to_csv to convert and made it more flexible. Will work with pretty much any delimiter.

--- lib/converter.ex
+++ lib/converter.ex
@@ -1,18 +1,19 @@
 defmodule Converter do
-	def csv_to_html path do
+	def convert path, delimiter do
 		{:ok, lines} = File.read(path)
 		lines = String.strip(lines)
 
 		{:ok, file} = File.open "result.html", [:write]
 		IO.binwrite file, "<html><body>" 
 							<> "<table border=\"1\">" 
-							<> get_rows lines <> "</table>" 
+							<> get_rows(lines, delimiter) <> "</table>" 
 							<> "</body></html>"
 	end
 	
-	defp get_rows lines do
+	defp get_rows(lines, delimiter) do
 		rows = String.split(lines, "\n")
-		tabled_cells = Enum.map(rows, fn row -> Enum.map(String.split(row, ","), fn cell -> "<td>#{cell}</td>" end) end)	
+		wrap_in_cell = fn cell -> "<td>#{cell}</td>" end
+		tabled_cells = Enum.map(rows, fn row -> Enum.map(String.split(row, delimiter), wrap_in_cell) end)	
 		Enum.join(Enum.map(tabled_cells, fn row -> "<tr>#{row}</tr>" end))
 	end
 end
