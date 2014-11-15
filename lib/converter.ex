5ab77466d05d31b6122ee2ada5c7160a68de9438
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2014-11-14 22:58:15 -0500

    Cleaned up results files. Application will now create a unique results file based on when the application was run.

--- lib/converter.ex
+++ lib/converter.ex
@@ -3,7 +3,7 @@ defmodule Converter do
 		{:ok, lines} = File.read(path)
 		lines = String.strip(lines)
 
-		{:ok, file} = File.open "result.html", [:write]
+		{:ok, file} = File.open "result-#{get_date_as_string}.html", [:write]
 		IO.binwrite file, "<html><body>" 
 							<> "<table border=\"1\">" 
 							<> get_rows(lines, delimiter) <> "</table>" 
@@ -15,5 +15,10 @@ defmodule Converter do
 		wrap_in_cell = fn cell -> "<td>#{cell}</td>" end
 		tabled_cells = Enum.map(rows, fn row -> Enum.map(String.split(row, delimiter), wrap_in_cell) end)	
 		Enum.join(Enum.map(tabled_cells, fn row -> "<tr>#{row}</tr>" end))
+	end
+
+	defp get_date_as_string do
+		{{year, month, day}, {hour, minute, second}} = :calendar.local_time
+		"#{year}-#{month}-#{day}_#{hour}-#{minute}-#{second}"
 	end
 end
