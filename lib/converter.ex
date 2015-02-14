117feb9ef2dd1cfb784f70260c8300cfccf26e3c
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-02-13 19:03:15 -0500

    added more test coverage for csv files

--- lib/converter.ex
+++ lib/converter.ex
@@ -8,30 +8,26 @@ defmodule Converter do
 	"""
 	def convert path, delimiter do
 		{:ok, lines} = File.read(path)
-		lines = String.strip(lines)
+		lines = String.split(lines, "\n")
 
 		{:ok, file} = File.open "result-#{get_date_as_string}.html", [:write]
-		IO.binwrite file, "<html><body>" 
-		IO.binwrite file, "<table border=\"1\">" 
-		IO.binwrite file, get_rows(lines, delimiter)
-		IO.binwrite file, "</table>" 
-		IO.binwrite file, "</body></html>"
+		html = generate_html(lines, delimiter)
+		IO.binwrite file, html
 	end
 	
-	defp get_rows(lines, delimiter) do
-		rows = String.split(lines, "\n")
-		wrap_in_cell = fn cell -> "<td>#{cell}</td>" end
-		tabled_cells = Enum.map(rows, fn row -> Enum.map(String.split(row, delimiter), wrap_in_cell) end)	
-		Enum.join(Enum.map(tabled_cells, fn row -> "<tr>#{row}</tr>" end))
-	end
-
 	defp get_date_as_string do
 		{{year, month, day}, {hour, minute, second}} = :calendar.local_time
 		"#{year}-#{month}-#{day}_#{hour}-#{minute}-#{second}"
 	end
 
-	def convert_row(csv_row) do
-		data = String.split(csv_row, ",")
+	def generate_html(lines, delimiter) do
+		table_rows = Enum.map(lines, fn line -> convert_row(line, delimiter) end)
+		concatenated_markup = Enum.join(table_rows)
+		"<html><body><table border=\"1\">#{concatenated_markup}</table></body></html>"
+	end
+
+	def convert_row(csv_row, delimiter) do
+		data = String.split(csv_row, delimiter)
 		|> Enum.map(fn datum -> "<td>#{datum}</td>" end) 
 		|> Enum.join
 		|> fn data -> "<tr>#{data}</tr>" end.()
--- test/converter_test.exs
+++ test/converter_test.exs
@@ -4,6 +4,13 @@ defmodule ConverterTest do
 	test "convert_row converts a csv row into an html table row" do
 		csv_row = "Jim Jones,19,Computer Science"
 		expected_html_table_row = "<tr><td>Jim Jones</td><td>19</td><td>Computer Science</td></tr>"
-		assert Converter.convert_row(csv_row) == expected_html_table_row
+		assert Converter.convert_row(csv_row, ",") == expected_html_table_row
+	end 
+
+	test "generate_html takes the csv lines and generates html for a web page and table" do
+		csv_row = ["Jim Jones,19,Computer Science"]
+		expected_html = "<html><body><table border=\"1\"><tr><td>Jim Jones</td><td>19</td><td>Computer Science</td></tr></table></body></html>"
+
+	  assert Converter.generate_html(csv_row, ",") == expected_html
 	end
 end
