4693538fe1e8f094434fcb3cba913ae56cefbbb6
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-02-14 21:49:50 -0500

    added test coverage for tsv files

--- test/converter_test.exs
+++ test/converter_test.exs
@@ -7,10 +7,23 @@ defmodule ConverterTest do
 		assert Converter.convert_row(csv_row, ",") == expected_html_table_row
 	end 
 
+	test "convert_row converts a tsv row into an html table row" do
+		csv_row = "Jim Jones\t19\tComputer Science"
+		expected_html_table_row = "<tr><td>Jim Jones</td><td>19</td><td>Computer Science</td></tr>"
+		assert Converter.convert_row(csv_row, "\t") == expected_html_table_row
+	end 
+
 	test "generate_html takes the csv lines and generates html for a web page and table" do
 		csv_row = ["Jim Jones,19,Computer Science"]
 		expected_html = "<html><body><table border=\"1\"><tr><td>Jim Jones</td><td>19</td><td>Computer Science</td></tr></table></body></html>"
 
 	  assert Converter.generate_html(csv_row, ",") == expected_html
+	end
+
+	test "generate_html takes the tsv lines and generates html for a web page and table" do
+		csv_row = ["Jim Jones\t19\tComputer Science"]
+		expected_html = "<html><body><table border=\"1\"><tr><td>Jim Jones</td><td>19</td><td>Computer Science</td></tr></table></body></html>"
+
+	  assert Converter.generate_html(csv_row, "\t") == expected_html
 	end
 end
